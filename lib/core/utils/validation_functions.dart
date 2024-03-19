b436a5f095567f7debd938617153319d1aacc2bc
airydev001 <josysmith02@gmail.com>
2024-03-19 11:22:46 -0700

    validation

--- lib/core/utils/validation_functions.dart
+++ lib/core/utils/validation_functions.dart
@@ -38,3 +38,69 @@ bool isText(
   }
   return isInputStringValid;
 }
+
+///check if string is phone number
+
+bool isValidPhone(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+
+  if (inputString != null && inputString.isNotEmpty) {
+    if (inputString.length > 16 || inputString.length < 6) return false;
+    const pattern = r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$';
+
+    final regExp = RegExp(pattern);
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
+
+bool isValidEmail(
+  String? inputString, {
+  bool isRequired = false,
+}) {
+  bool isInputStringValid = false;
+
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+  if (inputString != null && inputString.isNotEmpty) {
+    const pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9+\.)+[a-zA-Z]{2,}))$';
+
+    final regExp = RegExp(pattern);
+
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
+
+///Password should have
+///at least a upper case letter
+///at least a lower case letter
+///at least a digit
+///at leasrt a special character[@#$%^&+=]
+///length of at least 4
+///no white space allowed
+
+bool isValidPassword(String? inputString, {bool isRequired = false}) {
+  bool isInputStringValid = false;
+  if (!isRequired && (inputString == null ? true : inputString.isEmpty)) {
+    isInputStringValid = true;
+  }
+
+  if (inputString != null && inputString.isNotEmpty) {
+    const pattern =
+        r'^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$';
+
+    final regExp = RegExp(pattern);
+
+    isInputStringValid = regExp.hasMatch(inputString);
+  }
+  return isInputStringValid;
+}
